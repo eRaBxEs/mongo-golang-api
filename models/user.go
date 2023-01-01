c5c71884ee5a85bc3b1f04488a02bec23b4b1c98
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-01-01 23:54:34 +0100

    add models

---
+++ models/user.go
@@ -0,0 +1,12 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	Id     primitive.ObjectID `json:"id" bson:"_id"`
+	Name   string             `json:"name" bson:"name"`
+	Gender string             `json:"gender" bson:"gender"`
+	Age    int                `json:"age" bson:"age"`
+}
