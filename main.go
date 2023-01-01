d4b7540059b00864424cf43dfd1f274ec1ed0e3a
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-01-01 23:55:13 +0100

    add main code

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/erabxes/mongo-golang/controllers"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getClient())
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+func getClient() *mongo.Client {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	return client
+}
